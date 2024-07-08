a75bfbab46943768244dec0db9d21e742f70b0c2
kipland-m <kiplandmelton@gmail.com>
2024-07-07 20:03:42 -0500

    rewrote into separate functions to match solution as suggested

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+## Kipland Melton | boot.dev | 7/7/24
+
+def get_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    path_to_file = 'books/frankenstein.txt'
+    text = get_book(path_to_file)
+    words = get_words(text)
+
+    print(f"{words} words in given file")
+
+main()
