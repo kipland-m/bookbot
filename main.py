cf49520899c5a9bd953c0ae4e90fc39377fce551
Kipland Melton <kiplandmelton@gmail.com>
2024-07-07 22:36:57 -0500

    added function to create list of dicts, and to display report

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 ## Kipland Melton | boot.dev | 7/7/24
 
 def get_num_of_char(text):
-
     char_dict = {}
     chars_list = []
 
     for char in text:
         chars_list.append(char.lower())
-
     for char in chars_list:
         if char not in char_dict:
             char_dict[char] = 0
         if char in char_dict:
             char_dict[char] += 1
 
-    print(char_dict)
+    return char_dict
+
+def sort_on(dict):
+    return dict["occurence"]
+
+def create_list_of_dicts(char_dict):
+    char_dicts = []
+
+    for char, occurs in char_dict.items():
+        if char.isalpha():
+            new_dict = {"character": char, "occurence": occurs}
+            char_dicts.append(new_dict)
+        else:
+            continue
+
+    char_dicts.sort(reverse=True, key=sort_on)
+    return char_dicts
+
+def display_report(dict):
+    for item in dict:
+        print(f"The {item["character"]} char was found {item["occurence"]} times.")
+    
 
 def get_words(text):
     words = text.split()
@@ -29,10 +48,10 @@ def main():
     text = get_book(path_to_file)
     words = get_words(text)
     num_chars = get_num_of_char(text)
+    list_of_dicts = create_list_of_dicts(num_chars)
 
-
-    # print(f"{text}")
-    print(f"{words} words in given file")
-    print(num_chars)
-
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words in given file\n")
+    print(display_report(list_of_dicts))
+    
 main()
